733a4749720eec22cdb2bcce562553e81efb2e2f
junghwan Lee <kar00034@naver.com>
2019-12-30 12:52:57 +0900

    one_hot_encoding.py p.192 modify_dataframe_element.py p.? 남음 matplotlib_lines3.py 서울>대구 변경

--- data_structure/matplotlib_lines3.py
+++ data_structure/matplotlib_lines3.py
@@ -9,27 +9,26 @@ df = pd.read_excel('./시도별 전출입 인구수.xlsx',fillna=0, header=0)
 
 df = df.fillna(method='ffill')
 
-mask = (df['전출지별']=='서울특별시') & (df['전입지별']!='서울특별시')
+mask = (df['전출지별']=='대구광역시') & (df['전입지별']!='대구광역시')
 df_seoul = df[mask]
 df_seoul = df_seoul.drop(['전출지별'],axis=1)
 df_seoul.rename({'전입지별':'전입지'},axis=1, inplace=True)
 df_seoul.set_index('전입지',inplace=True)
 
-col_years = list(map(str,range(1970,2018)))
-df_3 = df_seoul.loc[['충청남도','경상북도','강원도'],col_years]
+col_years = list(map(str,range(1981,2018)))
+df_3 = df_seoul.loc[['서울특별시','경상북도','강원도'],col_years]
 
 plt.style.use('ggplot')
 
 fig = plt.figure(figsize=(20,5))
 ax = fig.add_subplot(1,1,1)
-
-ax.plot(col_years, df_3.loc['충청남도'],marker='o',markerfacecolor='green',markersize=5,color='olive',linewidth=2,label='서울 -> 충남')
-ax.plot(col_years, df_3.loc['경상북도'],marker='o',markerfacecolor='blue',markersize=5,color='skyblue',linewidth=2,label='서울 -> 경북')
-ax.plot(col_years, df_3.loc['강원도'],marker='o',markerfacecolor='red',markersize=5,color='magenta',linewidth=2,label='서울 -> 강원')
+ax.plot(col_years, df_3.loc['서울특별시'],marker='o',markerfacecolor='green',markersize=5,color='olive',linewidth=2,label='대구 -> 서울')
+ax.plot(col_years, df_3.loc['경상북도'],marker='o',markerfacecolor='blue',markersize=5,color='skyblue',linewidth=2,label='대구 -> 경북')
+ax.plot(col_years, df_3.loc['강원도'],marker='o',markerfacecolor='red',markersize=5,color='magenta',linewidth=2,label='대구 -> 강원')
 
 ax.legend(loc='best')
 
-ax.set_title('서울 -> 충남,경북,강원 인구 이동',size=20)
+ax.set_title('대구 -> 충남,경북,강원 인구 이동',size=20)
 
 ax.set_xlabel('기간',size=12)
 ax.set_ylabel('이동 인구수',size=12)
